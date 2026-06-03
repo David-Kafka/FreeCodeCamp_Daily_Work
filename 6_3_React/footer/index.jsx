9fa7137064dc2fd031e45dd055f4c037b600a6f9
David <79692191+David-Kafka@users.noreply.github.com>
2026-06-03 17:05:31 +0800

    Create index.jsx

---
+++ 6_3_React/footer/index.jsx
@@ -0,0 +1,23 @@
+export function Footer(){
+  return(
+    <footer>
+      <ul className="test">
+        <li>Help</li>
+        <li>Ask</li>
+        <li>More</li>
+      </ul>
+      <ul>
+        <li><a href="#">Personal Website</a></li>
+        <li><a href="#">Github</a></li>
+        <li><a href="#">Linkedin</a></li>
+      </ul>
+      <ul>
+        <li>Books</li>
+        <li>Games</li>
+        <li>Musics</li>
+      </ul>
+            <p>© 2026 David Kafka. All Rights Reserved.</p>
+    </footer>
+    
+  )
+}
